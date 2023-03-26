4acc36afb208b61376038f551bdac6944555f9fa
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-03-26 14:34:13 -0500

    Update Camioneta.java

--- src/main/src/vehiculos/Camioneta.java
+++ src/main/src/vehiculos/Camioneta.java
@@ -2,7 +2,7 @@ package vehiculos;
 
 public class Camioneta extends Vehiculo {
     private boolean volco;
-    private static int cantidadCamionetas = 0;
+    private static int cantidadCamionetas;
 
     public Camioneta(String placa, int puertas,String nombre, double precio, double peso, String traccion, Fabricante fabricante, boolean volco) {
         super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
