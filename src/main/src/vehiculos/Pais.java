ca8c373084220c5305bdbbcdfaf897504637e818
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-03-26 14:17:16 -0500

    Update Pais.java

--- src/main/src/vehiculos/Pais.java
+++ src/main/src/vehiculos/Pais.java
@@ -1,4 +1,5 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Pais {
 	private String nombre;
@@ -8,6 +9,20 @@ public class Pais {
 		this.nombre = nombre;
 		numeroVendidos++;
 	}
+	
+    public static Pais paisMasVendedor() {
+    	ArrayList<Fabricante> fabricantes = Vehiculo.getFabricantes();
+    	int mayorVendedor = 0;
+    	Pais paisMayor = null;
+    	for (int i = 0;i<fabricantes.size();i++) {
+    		Fabricante pfabricante = fabricantes.get(i);
+    		if (pfabricante.getPais().getNumeroVendidos()>mayorVendedor) {
+    			mayorVendedor = pfabricante.getPais().getNumeroVendidos();
+    			paisMayor = pfabricante.getPais();
+    		}
+    	}
+    	return paisMayor;
+    }
 
     public String getNombre() {
         return nombre;
@@ -15,6 +30,9 @@ public class Pais {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    public int getNumeroVendidos() {
+    	return numeroVendidos;
     }
 
 }
