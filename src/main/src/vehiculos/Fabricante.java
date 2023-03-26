c74c7ba1a3d0a86b0f8f21ec3d90264e4e7b45c4
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-03-26 13:50:35 -0500

    Update Fabricante.java

--- src/main/src/vehiculos/Fabricante.java
+++ src/main/src/vehiculos/Fabricante.java
@@ -3,10 +3,17 @@ package vehiculos;
 public class Fabricante {
 	private String nombre;
     private Pais pais;
+    private int numeroVendidos;
 
     public Fabricante(String nombre, Pais pais) {
         this.nombre = nombre;
         this.pais = pais;
+        numeroVendidos++;
+    }
+    
+    
+    public int getNumeroVendidos() {
+    	return numeroVendidos;
     }
 
     public String getNombre() {
