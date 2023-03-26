0e0f783f7074914180cb09788e157cd446c67b3f
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-03-26 14:34:07 -0500

    Update Automovil.java

--- src/main/src/vehiculos/Automovil.java
+++ src/main/src/vehiculos/Automovil.java
@@ -2,11 +2,10 @@ package vehiculos;
 
 public class Automovil extends Vehiculo{
     private int puestos;
-    private static int cantidadAutomoviles = 0;
+    private static int cantidadAutomoviles;
 
     
-    public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, 
-                     double precio, double peso, String traccion, Fabricante fabricante, int puestos) {
+    public Automovil( String placa, String nombre, double precio, double peso, Fabricante fabricante, int puestos) {
         super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
         this.puestos = puestos;
         cantidadAutomoviles++;
